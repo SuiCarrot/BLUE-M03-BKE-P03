c8fe0e0b7169e38f3cfca4c19e566142b0af8dc4
SuiCarrot <91708805+SuiCarrot@users.noreply.github.com>
2022-04-26 20:35:30 -0300

    feat(Routes): atualização de rotas

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const router = express.Router()
 
-const personagensController = require('../controllers/controller.js')
+
+const userController = require('../controllers/user-controller.js');
+const authController = require('../controllers/auth-controller');
+const personagensController = require('../controllers/controller.js');
+
 
 //USER
-router.get('/users', personagensController)
-router.create
+router.get('/users', userController.findUserController)
+router.post('/users/create', userController.createUserController)
 
 //AUTH
-
+router.post('auth/login', authController.findAuthLoginController)
 
 //PERSONAGENS
+router.get('/characters', personagensController.findPersonagensController);
 
+router.post('/characters/create', personagensController.createPersonagemController);
+
+router.get('/characters/find/:id', personagensController.findPersonagemByIdController);
+
+router.put('/characters/update/:id', personagensController.updatePersonagemController);
+
+router.delete('characters/delete/:id', personagensController.deletePersonagemController);
+
+router.get('/characters/search', personagensController.findPersonagemByNameController);
+
+module.exports = router
