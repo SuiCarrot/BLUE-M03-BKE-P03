5d41335056495f2c833b85ea4010c696e352203a
SuiCarrot <91708805+SuiCarrot@users.noreply.github.com>
2022-04-26 20:15:44 -0300

    feat: Services; Criaçao de arquivos separados

---
+++ src/routes/route.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+const personagensController = require('../controllers/controller.js')
+
+//USER
+router.get('/users', personagensController)
+router.create
+
+//AUTH
+
+
+//PERSONAGENS
+
