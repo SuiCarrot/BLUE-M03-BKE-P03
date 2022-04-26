8fd45690b4b8357c78139f6bea3c74dba8ceeb0f
ddanilooalves <daniloalves0512@gmail.com>
2022-04-25 23:00:10 -0300

    .

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const cors = require('cors');
-const route = require('./src/routes/tww.route.js');
+const route = require('./src/routes/route.js');
 const connectToDatabase = require('./src/database/database.js');
 
 const port = 3000;
-const app = express()
+const app = express();
 
-connectToDatabase()
+connectToDatabase();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.use('/', route);
 
 app.listen(port, ()=>{
     console.log(`Sevidor rodando em http://localhost:${port}`);
-}
-)
+})
 
 
 
